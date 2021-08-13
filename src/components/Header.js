56553fbd10d8424ec5ce04b18bf52798e9996e72
helciodev <helcioandre@live.com>
2021-08-13 15:15:10 +0100

    Make MemeGenerator a class component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Header () {
-  return(
-    <header>
-      I am the header
-    </header>
-  )
+class Header extends Component {
+  render() {
+
+    return(
+      <header>
+        I am the header
+      </header>
+    )
+  }
 }
 
 export default Header;
