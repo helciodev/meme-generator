20c4075fe3d77f17f5c44bba522c1297b930e426
helciodev <helcioandre@live.com>
2021-08-13 15:23:58 +0100

    Inialize state with bottomText, topText and randomImage

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,16 @@ class Header extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      topText: '',
+      bottomText: '',
+      randomImage: 'http://i.imgflip.com/1bij.jpg'
     }
   }
   render() {
     return(
       <header>
        <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="a troll face"/>
+       <p>meme generator</p>
       </header>
     )
   }
