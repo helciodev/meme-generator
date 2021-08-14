1d06a00ded56e8965c3749f65e1c1cf6649c7199
helciodev <helcioandre@live.com>
2021-08-14 13:23:19 +0100

    Set state for allMemeImgs to equal memes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Header extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {
-      topText: '',
-      bottomText: '',
-      randomImage: 'http://i.imgflip.com/1bij.jpg'
-    }
-  }
-  render() {
+function Header() {
     return(
       <header>
        <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="a troll face"/>
        <p>meme generator</p>
       </header>
     )
-  }
 }
 
 export default Header;
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function MemeGenerator() {
-  return(
-    <>
-    I am the meme generator
-    </>
-  )
+class MemeGenerator extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      topText: '',
+      bottomText: '',
+      randomImage: 'http://i.imgflip.com/1bij.jpg',
+      allMemeImgs: '',
+    }
+  }
+
+  componentDidMount() {
+      const api = 'https://api.imgflip.com/get_memes';
+      fetch(api).then(response => response.json()).then( response  => {
+        const {memes} = response.data 
+        this.setState({allMemeImgs: memes})
+        // console.log(this.state.allMemeImgs)
+       } )
+        
+    }
+
+  render() {
+
+    return(
+      <>
+      I am the meme generator
+      </>
+    )
+  }
 }
 
 export default MemeGenerator;
